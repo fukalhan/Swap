75e019c55ebdaa25346496629246f4280d16991c
Hana Fukalová <fukalhan@fit.cvut.cz>
2023-04-03 21:43:29 +0200

    FEAT: Add sign in and sign up screens

---
+++ feature/login/src/main/kotlin/cz/cvut/fukalhan/swap/login/system/LoginScreen.kt
@@ -0,0 +1,38 @@
+package cz.cvut.fukalhan.swap.login.system
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import cz.cvut.fukalhan.design.system.SwapAppTheme
+import cz.cvut.fukalhan.swap.login.R
+
+@Composable
+fun LoginScreen() {
+    var tabIndex by remember { mutableStateOf(0) }
+    val tabs = listOf(stringResource(R.string.signIn), stringResource(R.string.signUp))
+
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        TabRow(
+            selectedTabIndex = tabIndex,
+        ) {
+            tabs.forEachIndexed { index, title ->
+                Tab(text = { Text(title)},
+                    modifier = Modifier.background(SwapAppTheme.colors.secondary),
+                    selected = tabIndex == index,
+                    onClick = { tabIndex = index}
+                )
+            }
+        }
+
+        when(tabIndex) {
+            0 -> SignInScreen()
+            1 -> SignUpScreen()
+        }
+    }
+}
