29cbbc1cd7b95ab3f5426cd4e2d8a259aa8b2cb8
Hana Fukalová <fukalhan@fit.cvut.cz>
2023-04-18 18:08:40 +0200

    FIX: Change login state resolution, add loading state

--- feature/login/src/main/kotlin/cz/cvut/fukalhan/swap/login/presentation/signin/SignInResultExtension.kt
+++ feature/login/src/main/kotlin/cz/cvut/fukalhan/swap/login/presentation/signin/SignInResultExtension.kt
@@ -2,13 +2,15 @@ package cz.cvut.fukalhan.swap.login.presentation.signin
 
 import cz.cvut.fukalhan.swap.auth.model.signin.SignInResult
 import cz.cvut.fukalhan.swap.login.R
+import cz.cvut.fukalhan.swap.login.presentation.common.Failure
 import cz.cvut.fukalhan.swap.login.presentation.common.LoginState
+import cz.cvut.fukalhan.swap.login.presentation.common.Success
 
 fun SignInResult.toLoginState(): LoginState {
     return when (this) {
-        SignInResult.NOT_EXISTING_ACCOUNT -> LoginState(LoginState.State.FAILED, R.string.nonExistantAccount)
-        SignInResult.WRONG_PASSWORD -> LoginState(LoginState.State.FAILED, R.string.wrongPassword)
-        SignInResult.FAIL -> LoginState(LoginState.State.FAILED, R.string.loginFailed)
-        else -> LoginState(LoginState.State.SUCCESS, R.string.loginSuccess)
+        SignInResult.NOT_EXISTING_ACCOUNT -> Failure(R.string.nonExistantAccount)
+        SignInResult.WRONG_PASSWORD -> Failure(R.string.wrongPassword)
+        SignInResult.FAIL -> Failure(R.string.loginFailed)
+        else -> Success
     }
 }
--- feature/login/src/main/kotlin/cz/cvut/fukalhan/swap/login/presentation/signup/SignUpResultExtension.kt
+++ feature/login/src/main/kotlin/cz/cvut/fukalhan/swap/login/presentation/signup/SignUpResultExtension.kt
@@ -2,15 +2,17 @@ package cz.cvut.fukalhan.swap.login.presentation.signup
 
 import cz.cvut.fukalhan.swap.auth.model.signup.SignUpResult
 import cz.cvut.fukalhan.swap.login.R
+import cz.cvut.fukalhan.swap.login.presentation.common.Failure
 import cz.cvut.fukalhan.swap.login.presentation.common.LoginState
+import cz.cvut.fukalhan.swap.login.presentation.common.Success
 
 fun SignUpResult.toLoginState(): LoginState {
     return when (this) {
-        SignUpResult.SERVICE_UNAVAILABLE -> LoginState(LoginState.State.FAILED, R.string.serviceUnavailable)
-        SignUpResult.USERNAME_TAKEN -> LoginState(LoginState.State.FAILED, R.string.usernameTaken)
-        SignUpResult.WEAK_PASSWORD -> LoginState(LoginState.State.FAILED, R.string.weakPassword)
-        SignUpResult.EMAIL_ALREADY_REGISTERED -> LoginState(LoginState.State.FAILED, R.string.emailTaken)
-        SignUpResult.FAIL -> LoginState(LoginState.State.FAILED, R.string.loginFailed)
-        else -> LoginState(LoginState.State.SUCCESS, R.string.loginSuccess)
+        SignUpResult.SERVICE_UNAVAILABLE -> Failure(R.string.serviceUnavailable)
+        SignUpResult.USERNAME_TAKEN -> Failure(R.string.usernameTaken)
+        SignUpResult.WEAK_PASSWORD -> Failure(R.string.weakPassword)
+        SignUpResult.EMAIL_ALREADY_REGISTERED -> Failure(R.string.emailTaken)
+        SignUpResult.FAIL -> Failure(R.string.loginFailed)
+        else -> Success
     }
 }
