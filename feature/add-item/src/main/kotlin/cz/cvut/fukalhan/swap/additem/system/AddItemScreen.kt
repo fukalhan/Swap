5665b4cb9578c5711b4a85f7945ff5ec6e71a344
Hana Fukalová <fukalhan@fit.cvut.cz>
2023-04-18 21:38:30 +0200

    FEAT: Add loading state to add item screen

--- feature/add-item/src/main/kotlin/cz/cvut/fukalhan/swap/additem/system/AddItemScreen.kt
+++ feature/add-item/src/main/kotlin/cz/cvut/fukalhan/swap/additem/system/AddItemScreen.kt
@@ -1,17 +1,20 @@
 package cz.cvut.fukalhan.swap.additem.system
 
-import android.content.Context
 import android.net.Uri
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -22,8 +25,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.zIndex
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import cz.cvut.fukalhan.design.presentation.ScreenState
@@ -31,8 +36,9 @@ import cz.cvut.fukalhan.design.system.SwapAppTheme
 import cz.cvut.fukalhan.swap.additem.R
 import cz.cvut.fukalhan.swap.additem.presentation.AddItemState
 import cz.cvut.fukalhan.swap.additem.presentation.AddItemViewModel
-import cz.cvut.fukalhan.swap.additem.presentation.FailedState
-import cz.cvut.fukalhan.swap.additem.presentation.SuccessfulState
+import cz.cvut.fukalhan.swap.additem.presentation.Failure
+import cz.cvut.fukalhan.swap.additem.presentation.Loading
+import cz.cvut.fukalhan.swap.additem.presentation.Success
 import cz.cvut.fukalhan.swap.additem.system.helperviews.ButtonRow
 import cz.cvut.fukalhan.swap.additem.system.helperviews.CategoryList
 import cz.cvut.fukalhan.swap.additem.system.helperviews.DescriptionView
@@ -49,13 +55,22 @@ fun AddItemScreen(
     onScreenInit: (ScreenState) -> Unit,
     navigateBack: () -> Unit,
 ) {
-    val saveItemState by viewModel.saveItemState.collectAsState()
+    val addItemState by viewModel.addItemState.collectAsState()
 
     TopBar(onScreenInit)
 
-    DisplaySaveItemResponseMessage(viewModel, LocalContext.current, saveItemState, navigateBack)
-
-    ItemData(viewModel, navigateBack)
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        LoadingView(addItemState)
+        OnSuccessState(addItemState) {
+            viewModel.setStateToInit()
+            navigateBack()
+        }
+        OnFailState(addItemState)
+        ItemData(viewModel, navigateBack)
+    }
 }
 
 @Composable
@@ -73,19 +88,41 @@ fun TopBar(onScreenInit: (ScreenState) -> Unit) {
 }
 
 @Composable
-fun DisplaySaveItemResponseMessage(
-    viewModel: AddItemViewModel,
-    context: Context,
-    itemState: AddItemState,
+fun LoadingView(saveItemState: AddItemState) {
+    if (saveItemState is Loading) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black.copy(alpha = 0.5f))
+                .zIndex(1f),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .size(SwapAppTheme.dimensions.icon),
+                color = SwapAppTheme.colors.primary
+            )
+        }
+    }
+}
+
+@Composable
+fun OnSuccessState(
+    addItemState: AddItemState,
     navigateBack: () -> Unit
 ) {
-    viewModel.neutralizeItemState()
-    if (itemState is SuccessfulState || itemState is FailedState) {
-        Toast.makeText(context, stringResource(itemState.messageRes), Toast.LENGTH_SHORT).show()
-    }
-
-    if (itemState is SuccessfulState) {
+    if (addItemState is Success) {
+        val context = LocalContext.current
+        Toast.makeText(context, stringResource(addItemState.message), Toast.LENGTH_SHORT).show()
         navigateBack()
+    }
+}
+
+@Composable
+fun OnFailState(addItemState: AddItemState) {
+    if (addItemState is Failure) {
+        val context = LocalContext.current
+        Toast.makeText(context, stringResource(addItemState.message), Toast.LENGTH_SHORT).show()
     }
 }
 
@@ -123,6 +160,7 @@ fun ItemData(
                 .padding(bottom = SwapAppTheme.dimensions.sidePadding)
                 .fillMaxWidth()
                 .wrapContentHeight()
+                .zIndex(0f)
         ) {
             Column(
                 modifier = Modifier
