afa2dff7ca5b305c4f1b9aaf3e2f24e644e61126
Hana Fukalová <fukalhan@fit.cvut.cz>
2023-04-18 21:38:30 +0200

    FEAT: Add loading state to add item screen

--- library/item-data/src/main/kotlin/cz/cvut/fukalhan/swap/itemdata/domain/SaveItemUseCase.kt
+++ library/item-data/src/main/kotlin/cz/cvut/fukalhan/swap/itemdata/domain/SaveItemUseCase.kt
@@ -1,5 +1,7 @@
 package cz.cvut.fukalhan.swap.itemdata.domain
 
+import cz.cvut.fukalhan.swap.itemdata.data.Response
+import cz.cvut.fukalhan.swap.itemdata.data.ResponseFlag
 import cz.cvut.fukalhan.swap.itemdata.data.SaveImagesRequest
 import cz.cvut.fukalhan.swap.itemdata.domain.repo.ImageRepository
 import cz.cvut.fukalhan.swap.itemdata.domain.repo.ItemRepository
@@ -9,22 +11,22 @@ class SaveItemUseCase(
     private val itemRepository: ItemRepository,
     private val imageRepository: ImageRepository
 ) {
-    suspend fun saveItem(item: Item): SaveItemResponse {
+    suspend fun saveItem(item: Item): Response<ResponseFlag> {
         val createItemRecordResult = itemRepository.createItemRecord(item)
         createItemRecordResult.data?.let { itemId ->
             val saveItemImagesResult = imageRepository.saveItemImages(itemId, item.imagesUri)
             saveItemImagesResult.data?.let { imagesUri ->
                 val updateItemImagesResult = itemRepository.updateItemImages(SaveImagesRequest(itemId, imagesUri))
                 if (updateItemImagesResult.success) {
-                    return SaveItemResponse.SUCCESS
+                    return Response(true, ResponseFlag.SUCCESS)
                 } else {
-                    return SaveItemResponse.FAIL
+                    return Response(false, ResponseFlag.FAIL)
                 }
             } ?: run {
-                return SaveItemResponse.FAIL
+                return Response(false, ResponseFlag.FAIL)
             }
         } ?: run {
-            return SaveItemResponse.FAIL
+            return Response(false, ResponseFlag.FAIL)
         }
     }
 }
