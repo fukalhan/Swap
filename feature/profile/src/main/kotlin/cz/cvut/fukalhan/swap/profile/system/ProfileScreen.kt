a3d8023b0cb60d9815bc2a8942d4eae6a62eee70
Hana Fukalová <fukalhan@fit.cvut.cz>
2023-04-18 14:43:52 +0200

    FEAT: Fix display of top app bar

--- feature/profile/src/main/kotlin/cz/cvut/fukalhan/swap/profile/system/ProfileScreen.kt
+++ feature/profile/src/main/kotlin/cz/cvut/fukalhan/swap/profile/system/ProfileScreen.kt
@@ -5,18 +5,50 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import cz.cvut.fukalhan.design.system.ScreenState
 import cz.cvut.fukalhan.design.system.SwapAppTheme
+import cz.cvut.fukalhan.swap.profile.R
 import cz.cvut.fukalhan.swap.profile.presentation.ProfileViewModel
 import cz.cvut.fukalhan.swap.profile.presentation.useritems.UserItemsViewModel
 
 @Composable
 fun ProfileScreen(
     profileViewModel: ProfileViewModel,
-    userItemsViewModel: UserItemsViewModel
+    userItemsViewModel: UserItemsViewModel,
+    onComposing: (ScreenState) -> Unit,
+    onSettingClick: () -> Unit
 ) {
+    LaunchedEffect(key1 = true) {
+        onComposing(
+            ScreenState {
+                Text(
+                    text = stringResource(R.string.profile),
+                    style = SwapAppTheme.typography.screenTitle,
+                    color = SwapAppTheme.colors.buttonText,
+                    modifier = Modifier.padding(start = SwapAppTheme.dimensions.sidePadding)
+                )
+                IconButton(onClick = onSettingClick) {
+                    Icon(
+                        painter = painterResource(R.drawable.settings),
+                        contentDescription = null,
+                        tint = SwapAppTheme.colors.buttonText,
+                        modifier = Modifier.size(SwapAppTheme.dimensions.icon)
+                    )
+                }
+            }
+        )
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
