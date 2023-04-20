aac26a59c2facef7012e8f4cbf3b42ae5e9bb195
Hana Fukalová <fukalhan@fit.cvut.cz>
2023-04-20 14:50:19 +0200

    FEAT: Add updating of likes in the items list

--- library/item-data/src/main/kotlin/cz/cvut/fukalhan/swap/itemdata/domain/GetItemsUseCase.kt
+++ library/item-data/src/main/kotlin/cz/cvut/fukalhan/swap/itemdata/domain/GetItemsUseCase.kt
@@ -6,7 +6,17 @@ import cz.cvut.fukalhan.swap.itemdata.domain.repo.ItemRepository
 import cz.cvut.fukalhan.swap.itemdata.model.Item
 
 class GetItemsUseCase(private val itemRepository: ItemRepository) {
-    suspend fun getItems(uid: String): DataResponse<ResponseFlag, List<Item>> {
-        return itemRepository.getItems(uid)
+    suspend fun getItems(uid: String): DataResponse<ResponseFlag, Pair<List<Item>, List<String>>> {
+        val getItemsResponse = itemRepository.getItems(uid)
+        val getItemsLikedByUserResponse = itemRepository.getItemsLikedByUser(uid)
+        return if (getItemsResponse.data != null && getItemsLikedByUserResponse.data != null) {
+            DataResponse(
+                true,
+                ResponseFlag.SUCCESS,
+                Pair(getItemsResponse.data, getItemsLikedByUserResponse.data)
+            )
+        } else {
+            DataResponse(false, ResponseFlag.FAIL)
+        }
     }
 }
