0a49e45cd645d5260e14a35513f1319b4d549980
Hana Fukalová <fukalhan@fit.cvut.cz>
2023-04-23 15:02:23 +0200

    FEAT: Add custom chat theme with custom colors

--- library/design/src/main/kotlin/cz/cvut/fukalhan/design/system/Colors.kt
+++ library/design/src/main/kotlin/cz/cvut/fukalhan/design/system/Colors.kt
@@ -3,6 +3,7 @@ package cz.cvut.fukalhan.design.system
 import androidx.compose.ui.graphics.Color
 
 val white = Color.White
+val red = Color.Red
 val beige = Color(0xfff7ead8)
 val peach = Color(0xffffd185)
 val darkPeach = Color(0xffffbb5c)
