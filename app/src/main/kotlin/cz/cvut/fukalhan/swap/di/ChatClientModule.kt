920846215732ef2ffb20916910a3452090fc2da8
Hana Fukalová <fukalhan@fit.cvut.cz>
2023-04-23 18:02:38 +0200

    FEAT: Add config for push notifications

--- app/src/main/kotlin/cz/cvut/fukalhan/swap/di/ChatClientModule.kt
+++ app/src/main/kotlin/cz/cvut/fukalhan/swap/di/ChatClientModule.kt
@@ -2,9 +2,11 @@ package cz.cvut.fukalhan.swap.di
 
 import cz.cvut.fukalhan.swap.BuildConfig
 import io.getstream.chat.android.client.ChatClient
+import io.getstream.chat.android.client.notifications.handler.NotificationConfig
 import io.getstream.chat.android.offline.model.message.attachments.UploadAttachmentsNetworkType
 import io.getstream.chat.android.offline.plugin.configuration.Config
 import io.getstream.chat.android.offline.plugin.factory.StreamOfflinePluginFactory
+import io.getstream.chat.android.pushprovider.firebase.FirebasePushDeviceGenerator
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
@@ -22,7 +24,12 @@ val chatClientModule = module {
             ),
             appContext = context,
         )
+        val notificationConfig = NotificationConfig(
+            pushDeviceGenerators = listOf(FirebasePushDeviceGenerator())
+        )
+
         ChatClient.Builder(BuildConfig.STREAM_CHAT_API_KEY, get())
+            .notifications(notificationConfig)
             .withPlugin(pluginFactory)
             .build()
     }
