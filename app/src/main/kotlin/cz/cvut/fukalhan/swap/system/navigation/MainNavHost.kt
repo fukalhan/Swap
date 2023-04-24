213ac49e012a1f1eb3a25c84bd952231cf3522c0
Hana Fukalová <fukalhan@fit.cvut.cz>
2023-04-24 16:12:41 +0200

    FEAT: Add display item detail from chat screen item view

--- app/src/main/kotlin/cz/cvut/fukalhan/swap/system/navigation/MainNavHost.kt
+++ app/src/main/kotlin/cz/cvut/fukalhan/swap/system/navigation/MainNavHost.kt
@@ -128,6 +128,7 @@ fun MainNavHost(
                     ChatScreen(
                         channelId,
                         getKoin().get(),
+                        navController,
                         { screenState = it }
                     ) {
                         navController.popBackStack()
