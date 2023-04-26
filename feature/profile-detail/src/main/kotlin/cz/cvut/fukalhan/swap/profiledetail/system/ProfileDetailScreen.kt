58ef83343c13488c653fad16a3e9948bc6fc2007
Hana Fukalová <fukalhan@fit.cvut.cz>
2023-04-26 17:50:24 +0200

    FEAT: Add navigate from review to reviewer profile

--- feature/profile-detail/src/main/kotlin/cz/cvut/fukalhan/swap/profiledetail/system/ProfileDetailScreen.kt
+++ feature/profile-detail/src/main/kotlin/cz/cvut/fukalhan/swap/profiledetail/system/ProfileDetailScreen.kt
@@ -1,20 +1,24 @@
 package cz.cvut.fukalhan.swap.profiledetail.system
 
+import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -28,10 +32,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import cz.cvut.fukalhan.design.presentation.ScreenState
 import cz.cvut.fukalhan.design.system.SwapAppTheme
 import cz.cvut.fukalhan.design.system.components.UserInfoView
@@ -54,7 +63,8 @@ fun ProfileDetailScreen(
     userId: String,
     viewModel: ProfileDetailViewModel,
     onInitScreen: (ScreenState) -> Unit,
-    navigateBack: () -> Unit
+    navigateBack: () -> Unit,
+    navigateToProfileDetail: (String) -> Unit
 ) {
     val profileDetailState by viewModel.profileDetailState.collectAsState()
     val effect = remember {
@@ -73,7 +83,7 @@ fun ProfileDetailScreen(
             .fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        ResolveState(profileDetailState)
+        ResolveState(profileDetailState, navigateToProfileDetail)
     }
 }
 
@@ -108,17 +118,23 @@ fun TopBar(
 }
 
 @Composable
-fun ResolveState(state: ProfileDetailState) {
+fun ResolveState(
+    state: ProfileDetailState,
+    navigateToProfileDetail: (String) -> Unit
+) {
     when (state) {
         is Loading -> LoadingView(semiTransparentBlack)
-        is Success -> ProfileDetail(state)
+        is Success -> ProfileDetail(state, navigateToProfileDetail)
         is Failure -> FailureView(state.message)
         else -> {}
     }
 }
 
 @Composable
-fun ProfileDetail(state: Success) {
+fun ProfileDetail(
+    state: Success,
+    navigateToProfileDetail: (String) -> Unit
+) {
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
@@ -131,7 +147,8 @@ fun ProfileDetail(state: Success) {
         )
         ReviewList(
             state.reviews,
-            Modifier.weight(1f)
+            Modifier.weight(1f),
+            navigateToProfileDetail
         )
     }
 }
@@ -155,7 +172,8 @@ fun Bio(bio: String) {
 @Composable
 fun ReviewList(
     reviews: List<ReviewState>,
-    modifier: Modifier
+    modifier: Modifier,
+    navigateToProfileDetail: (String) -> Unit
 ) {
     Text(
         text = stringResource(R.string.reviews),
@@ -180,7 +198,7 @@ fun ReviewList(
                     .fillMaxSize()
             ) {
                 items(reviews) {
-                    ReviewCard(it)
+                    ReviewCard(it, navigateToProfileDetail)
                     Divider(
                         modifier = Modifier
                             .fillMaxWidth()
@@ -193,9 +211,12 @@ fun ReviewList(
 }
 
 @Composable
-fun ReviewCard(review: ReviewState) {
+fun ReviewCard(
+    review: ReviewState,
+    navigateToProfileDetail: (String) -> Unit
+) {
     var expanded by remember { mutableStateOf(false) }
-    val modifier = if (expanded) Modifier.wrapContentHeight() else Modifier.height(75.dp)
+    val modifier = if (expanded) Modifier.wrapContentHeight() else Modifier.height(90.dp)
 
     Column(
         modifier = modifier
@@ -203,7 +224,15 @@ fun ReviewCard(review: ReviewState) {
             .padding(SwapAppTheme.dimensions.smallSidePadding)
             .clickable { expanded = !expanded }
     ) {
-        Rating(review.rating)
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            UserProfilePic(review.reviewerProfilePic) {
+                navigateToProfileDetail(review.reviewerId)
+            }
+            Spacer(modifier = Modifier.width(SwapAppTheme.dimensions.smallSpacer))
+            Rating(review.rating)
+        }
         Text(
             modifier = Modifier
                 .padding(SwapAppTheme.dimensions.sidePadding)
@@ -215,6 +244,27 @@ fun ReviewCard(review: ReviewState) {
             overflow = TextOverflow.Ellipsis
         )
     }
+}
+
+@Composable
+fun UserProfilePic(
+    uri: Uri,
+    navigateToProfileDetail: () -> Unit
+) {
+    AsyncImage(
+        model = ImageRequest.Builder(LocalContext.current)
+            .data(uri)
+            .placeholder(cz.cvut.fukalhan.design.R.drawable.profile_pic_placeholder)
+            .crossfade(true)
+            .build(),
+        placeholder = painterResource(cz.cvut.fukalhan.design.R.drawable.profile_pic_placeholder),
+        contentDescription = null,
+        modifier = Modifier
+            .clip(CircleShape)
+            .size(40.dp)
+            .clickable(onClick = navigateToProfileDetail),
+        contentScale = ContentScale.Fit
+    )
 }
 
 @Composable
