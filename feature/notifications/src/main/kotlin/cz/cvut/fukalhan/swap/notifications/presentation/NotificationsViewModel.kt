ea955ec456ce774d1027849a33e89e7159b38e4e
Hana Fukalová <fukalhan@fit.cvut.cz>
2023-04-27 23:22:01 +0200

    FEAT: Add count of new notifications to notification icon button in the profile top bar

--- feature/notifications/src/main/kotlin/cz/cvut/fukalhan/swap/notifications/presentation/NotificationsViewModel.kt
+++ feature/notifications/src/main/kotlin/cz/cvut/fukalhan/swap/notifications/presentation/NotificationsViewModel.kt
@@ -1,10 +1,10 @@
 package cz.cvut.fukalhan.swap.notifications.presentation
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import cz.cvut.fukalhan.design.presentation.StringResources
 import cz.cvut.fukalhan.swap.userdata.domain.GetNotificationUseCase
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -17,18 +17,23 @@ class NotificationsViewModel(
     val notifications: StateFlow<List<NotificationState>>
         get() = _notifications
 
+    private val _newNotificationsCount = MutableStateFlow(0)
+    val newNotificationsCount: StateFlow<Int>
+        get() = _newNotificationsCount
+
     private val _notificationState: MutableStateFlow<NotificationsState> = MutableStateFlow(Init)
     val notificationsState: StateFlow<NotificationsState>
         get() = _notificationState
 
     fun getNotification(userId: String, itemId: String) {
         _notificationState.value = Loading
-        viewModelScope.launch(Dispatchers.IO) {
+        MainScope().launch(Dispatchers.IO) {
             val response = getNotificationUseCase.getNotification(userId, itemId)
             response.data?.let {
                 val notification = it.toNotificationState(stringResources)
                 _notificationState.value = it.toNotificationState(stringResources)
                 _notifications.value += notification.data
+                _newNotificationsCount.value += 1
             } ?: run {
                 _notificationState.value = Failure()
             }
@@ -37,5 +42,9 @@ class NotificationsViewModel(
 
     fun setStateToInit() {
         _notificationState.value = Init
+    }
+
+    fun resetNewNotificationsCount() {
+        _newNotificationsCount.value = 0
     }
 }
--- feature/profile/src/main/kotlin/cz/cvut/fukalhan/swap/profile/system/ProfileScreen.kt
+++ feature/profile/src/main/kotlin/cz/cvut/fukalhan/swap/profile/system/ProfileScreen.kt
@@ -1,6 +1,8 @@
 package cz.cvut.fukalhan.swap.profile.system
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -8,18 +10,26 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import cz.cvut.fukalhan.design.presentation.ScreenState
 import cz.cvut.fukalhan.design.system.SwapAppTheme
+import cz.cvut.fukalhan.swap.notifications.presentation.NotificationsViewModel
 import cz.cvut.fukalhan.swap.profile.R
 import cz.cvut.fukalhan.swap.profile.system.items.ItemsView
 import cz.cvut.fukalhan.swap.profile.system.profileinfo.ProfileInfo
@@ -27,13 +37,25 @@ import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun ProfileScreen(
+    viewModel: NotificationsViewModel,
     navController: NavHostController,
     onScreenInit: (ScreenState) -> Unit,
     navigateToNotifications: () -> Unit,
     onSettingsClick: () -> Unit,
     navigateToProfileDetail: (String) -> Unit
 ) {
-    TopBar(onScreenInit, navigateToNotifications, onSettingsClick)
+    val newNotifications by viewModel.newNotificationsCount.collectAsState()
+
+    TopBar(
+        onScreenInit,
+        {
+            viewModel.resetNewNotificationsCount()
+            navigateToNotifications()
+        },
+        onSettingsClick,
+        newNotifications,
+    )
+
     Profile(navController, navigateToProfileDetail)
 }
 
@@ -41,7 +63,8 @@ fun ProfileScreen(
 fun TopBar(
     onScreenInit: (ScreenState) -> Unit,
     navigateToNotifications: () -> Unit,
-    onSettingClick: () -> Unit
+    onSettingClick: () -> Unit,
+    newNotificationsCount: Int,
 ) {
     onScreenInit(
         ScreenState {
@@ -58,13 +81,36 @@ fun TopBar(
                     .fillMaxHeight(),
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                IconButton(onClick = navigateToNotifications) {
-                    Icon(
-                        painter = painterResource(R.drawable.notifications),
-                        contentDescription = null,
-                        tint = SwapAppTheme.colors.buttonText,
-                        modifier = Modifier.size(SwapAppTheme.dimensions.icon)
-                    )
+                Box(
+                    modifier = Modifier
+                        .wrapContentWidth()
+                        .fillMaxHeight()
+                ) {
+                    IconButton(onClick = navigateToNotifications) {
+                        Icon(
+                            painter = painterResource(R.drawable.notifications),
+                            contentDescription = null,
+                            tint = SwapAppTheme.colors.buttonText,
+                            modifier = Modifier.size(SwapAppTheme.dimensions.icon)
+                        )
+                    }
+                    if (newNotificationsCount > 0) {
+                        Box(
+                            modifier = Modifier
+                                .padding(SwapAppTheme.dimensions.smallSidePadding)
+                                .clip(CircleShape)
+                                .background(Color.Red)
+                                .size(15.dp)
+                                .align(Alignment.BottomStart),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(
+                                text = newNotificationsCount.toString(),
+                                style = SwapAppTheme.typography.smallText,
+                                modifier = Modifier.wrapContentSize()
+                            )
+                        }
+                    }
                 }
 
                 IconButton(onClick = onSettingClick) {
