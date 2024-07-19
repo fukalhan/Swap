7ccc56960da26b6e1f3d970562be9e38c39ac286
Hana Fukalová <hfukalova@csob.cz>
2024-07-19 15:42:34 +0200

    💄 Change ItemCard layout

--- library/design/src/main/kotlin/cz/cvut/fukalhan/design/system/components/ItemCard.kt
+++ library/design/src/main/kotlin/cz/cvut/fukalhan/design/system/components/ItemCard.kt
@@ -6,7 +6,8 @@ import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Surface
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -17,11 +18,15 @@ fun ItemCard(
     onClick: () -> Unit,
     content: @Composable ColumnScope.() -> Unit,
 ) {
-    Surface(
-        elevation = SwapAppTheme.dimensions.elevation,
-        color = SwapAppTheme.colors.background,
+    Card(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+        colors = CardDefaults.cardColors(
+            containerColor = SwapAppTheme.colors.background
+        ),
         modifier = Modifier
-            .padding(SwapAppTheme.dimensions.smallSidePadding)
+            .padding(6.dp)
             .clickable(onClick = onClick)
     ) {
         Column(
